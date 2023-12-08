b3890d729cfa7623c6d63615c41d9394e439b4ca
Ankur <annkur020@gmail.com>
2023-12-08 18:08:59 +0530

    utils created

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-
 // require("dotenv").config({ path: "./env" });
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Listening on Port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection Failed!!", err);
+  });
 
 /*
 import mongoose from "mongoose";
